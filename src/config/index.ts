85ff7dff7481513f2e3863a7638a29ed16eac1c9
Dadario23 <dandrada23@gmail.com>
2024-04-19 12:20:55 -0300

    edit conexion database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,10 @@
 import { Sequelize } from "sequelize";
 require("dotenv").config();
 
-const db = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    //host: "host.docker.internal",
-    // host: "192.168.1.33",
-    //host: "172.17.0.1",
-    // host: "127.0.0.1",
-    host: "postgres",
-    dialect: "postgres",
-    port: 5432,
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
 
 db.authenticate()
   .then(() => {
